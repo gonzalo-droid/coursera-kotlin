43984943457da722fbee997e796ccdfabbd9e31a
yodaRoot <gonzalolopezguerrero2050@gmail.com>
2021-07-27 12:16:01 -0500

    Pull

--- Nice String/Task/src/nicestring/NiceString.kt
+++ Nice String/Task/src/nicestring/NiceString.kt
@@ -1,30 +1,28 @@
 package nicestring
 
+import java.security.KeyStore
+
 fun String.isNice(): Boolean {
-    fun containsDoubleLetters(): Boolean {
-        for ((index, ch) in this.withIndex()) {
-            //solo un caracter
-            if (index + 1 == this.length) return false
-            //caracteres consecutivos iguales
-            if (ch == this[index + 1]) return true
+    val noBadString = !contains("ba") && !contains("be") && !contains("bu")
+    val hasThreeVowels = count{
+        it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u'
+    } >= 3
+
+    var hasDouble = false
+    if(length > 1 ){
+        var prevCh: Char? = null
+        this.forEach { ch ->
+            if(ch == prevCh )
+                hasDouble = true
+            prevCh = ch
         }
-        return false
     }
 
-    fun containsThreeVowels():Boolean{
-        val vowels = listOf('a','b','c','o','u')
-        return filter {
-            it -> vowels.contains(it)
-        }.count() >= 3
-    }
+    var conditions= 0
+    if(noBadString) conditions++
+    if(hasThreeVowels) conditions++
+    if(hasDouble) conditions++
 
-    fun containsSubstrings(): Boolean {
-        val substrings = listOf("bu", "ba", "be")
-        substrings.forEach { substring -> if (contains(substring)) return true }
-        return false;
-    }
-
-    return listOf(containsDoubleLetters(), !containsSubstrings(), containsThreeVowels())
-        .filter { it }
-        .count() >= 2
+    if(conditions >= 2 ) return true
+    return false
 }
